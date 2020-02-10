451af252c40b80d2a602d06de1e6cb086878c3e3
kumararchana <archana@archanakumar.com>
2020-02-10 23:55:00 +0530

    Tab -  using ngswitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NgForLoopComponent } from './ng-for-loop/ng-for-loop.component';
 import { EvenBindingComponent } from './even-binding-ng-class/even-binding.component';
+import {TabsNgswitchComponent} from './tabs-ngswitch/tabs-ngswitch.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NgForLoopComponent, EvenBindingComponent ],
+  declarations: [ AppComponent, HelloComponent, NgForLoopComponent, EvenBindingComponent, TabsNgswitchComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/tabs-ngswitch/tabs-ngswitch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-ngswitch',
+  templateUrl: './tabs-ngswitch.component.html',
+  styleUrls: ['./tabs-ngswitch.component.css']
+})
+export class TabsNgswitchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
