75daad8d3071ddd2f124a3358bc06073b5120d8f
kumararchana <archana@archanakumar.com>
2020-03-05 23:08:55 +0530

    Modal popup by ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NgForLoopComponent } from './ng-for-loop/ng-for-loop.component';
 import { EvenBindingComponent } from './even-binding-ng-class/even-binding.component';
 import {TabsNgswitchComponent} from './tabs-ngswitch/tabs-ngswitch.component';
+import { ModalPopupComponent } from './modal-popup/modal-popup.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NgForLoopComponent, EvenBindingComponent, TabsNgswitchComponent],
+  imports:      [ BrowserModule, FormsModule, NgbModule ],
+  declarations: [ AppComponent, HelloComponent, NgForLoopComponent, EvenBindingComponent, TabsNgswitchComponent, ModalPopupComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/modal-popup/modal-popup.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal-popup',
+  templateUrl: './modal-popup.component.html',
+  styleUrls: ['./modal-popup.component.css']
+})
+export class ModalPopupComponent {
+closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+}
+
+export class NgbdModalBasic {
+  
+}
