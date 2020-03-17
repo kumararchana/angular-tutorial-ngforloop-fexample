a48ad45491d0a7ca6a8da423460ed4f12e4a41af
kumararchana <archana@archanakumar.com>
2020-03-17 19:27:07 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -9,10 +10,21 @@ import { NgForLoopComponent } from './ng-for-loop/ng-for-loop.component';
 import { EvenBindingComponent } from './even-binding-ng-class/even-binding.component';
 import {TabsNgswitchComponent} from './tabs-ngswitch/tabs-ngswitch.component';
 import { ModalPopupComponent } from './modal-popup/modal-popup.component';
+import { NavBarComponentComponent } from './nav-bar-component/nav-bar-component.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, NgbModule ],
-  declarations: [ AppComponent, HelloComponent, NgForLoopComponent, EvenBindingComponent, TabsNgswitchComponent, ModalPopupComponent],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    NgbModule, 
+    RouterModule.forRoot([
+      {path : "tableByNgLoop", component : NgForLoopComponent},
+      {path: "tabsByNgswitch", component: TabsNgswitchComponent},
+      {path: "modalByNgbootstrap", component: ModalPopupComponent},
+      {path: "showHideByNgclass", component: EvenBindingComponent}
+    ]
+   ) ],
+  declarations: [ AppComponent, HelloComponent, NgForLoopComponent, EvenBindingComponent, TabsNgswitchComponent, ModalPopupComponent, NavBarComponentComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
